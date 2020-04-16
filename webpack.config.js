0dd4b8ce71bd8a239afcf17cad69f9896ced74d7
Jason Wang <jwang2@outlook.com>
2020-04-16 15:54:59 -0700

    pre AutoPrefixing

--- webpack.config.js
+++ webpack.config.js
@@ -1,8 +1,10 @@
-//adding html-webpack-plugin const
+const MiniCssExtractPlugin = require('mini-css-extract-plugin');
 const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 const env = process.env.NODE_ENV || 'development';
 // set to 'production' or 'development' in your env
+
+const finalCSSLoader = (env === 'production') ? MiniCssExtractPlugin.loader : { loader: 'style-loader' };
 
 module.exports = {
   mode: env,
@@ -13,16 +15,51 @@ module.exports = {
       {
         test: /\.js$/,
         exclude: /node_modules/,
-        use: [ //here's the part you want
-        { loader: 'babel-loader'}
-      ]
+        use: [
+          { loader: 'babel-loader' },
+          { loader: 'eslint-loader' },
+        ],
+      },
+      {
+        test: /\.s?css/,
+        use: [
+          finalCSSLoader,
+          {
+            loader: 'css-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+          {
+            loader: 'sass-loader',
+            options: {
+              sourceMap: true,
+            },
+          },
+        ],
+      },
+      {
+        test: /\.(jpe?g|png|gif|svg)$/,
+        use: [
+          {
+            loader: 'file-loader',
+            options: {
+              useRelativePath: true,
+              name: '[name].[ext]',
+            },
+          },
+        ],
       },
     ],
   },
   plugins: [
+    new MiniCssExtractPlugin(),
     new HtmlWebpackPlugin({
-        template: './src/index.html',
-        filename: './index.html',
-      }),
+      template: './src/index.html',
+      filename: './index.html',
+    }),
   ],
+  devServer: {
+      hot: true,
+  },
 };
