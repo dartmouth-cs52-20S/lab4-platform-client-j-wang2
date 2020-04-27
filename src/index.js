f6101b3a34443b61c33d26f060221922629943d5
Jason Wang <jwang2@outlook.com>
2020-04-27 10:19:44 -0700

    router importing

--- src/index.js
+++ src/index.js
@@ -1,7 +1,26 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
+import { BrowserRouter as Router, Route, NavLink } from 'react-router-dom';
 import './style.scss';
 
-const App = () => <div className="test">All the REACT are belong to us!</div>;
+const App = (props) => {
+    return (
+      <Router>
+        <div>
+          <Nav />
+          <Route exact path="/" component={Welcome} />
+          <Route path="/about" component={About} />
+        </div>
+      </Router>
+    );
+};
+
+const About = (props) => {
+    return <div> All there is to know about me </div>;
+};
+
+const Welcome = (props) => {
+    return <div>Welcome</div>;
+};
 
 ReactDOM.render(<App />, document.getElementById('main'));
