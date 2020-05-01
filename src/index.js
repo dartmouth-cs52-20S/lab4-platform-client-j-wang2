9801b8470af1006dc14362fb0b7c36a81aa449ec
Jason Wang <jwang2@outlook.com>
2020-04-30 18:06:27 -0700

    SA6 part1 fixed linting

--- src/index.js
+++ src/index.js
@@ -6,7 +6,7 @@ import { Provider } from 'react-redux';
 import { createStore, applyMiddleware, compose } from 'redux';
 import reducers from './reducers';
 
-import App from './components/app';
+import App from './components/App';
 
 // this creates the store with the reducers, and does some other stuff to initialize devtools
 // boilerplate to copy, don't have to know
